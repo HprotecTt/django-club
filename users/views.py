de2dec2551ab4e588ddfba7abba75dc2848ebd64
AbyssLink <26038696+AbyssLink@users.noreply.github.com>
2020-05-06 11:23:54 +0800

    add login function

--- users/views.py
+++ users/views.py
@@ -14,8 +14,9 @@ def register(request):
             # saving the user
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('club-home')
+            messages.success(
+                request, f'Your account has been created! You are now able to log In!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
