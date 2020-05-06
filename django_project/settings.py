d7ec647a718046bd74485fe3bdd2e5df460c6ac2
AbyssLink <26038696+AbyssLink@users.noreply.github.com>
2020-05-06 11:45:01 +0800

    add logout and profile function

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'club-home'
+
+LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
 
@@ -20,3 +21,8 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
