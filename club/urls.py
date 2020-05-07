fc02cb1697c6bc2ec7ca9d0c506a994f3c8e1689
AbyssLink <26038696+AbyssLink@users.noreply.github.com>
2020-05-07 13:24:14 +0800

    add post_list_view and post_detail_view

--- club/urls.py
+++ club/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='club-home'),
+    path('', PostListView.as_view(), name='club-home'),
+    # pk means primary key
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='club-about'),
 ]
--- club/views.py
+++ club/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -10,6 +11,18 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'club/home.html', contex)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'club/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    # inverse order as date_posted
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
