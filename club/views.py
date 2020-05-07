3cbc311e05239047ed51342c97bc02853ecd0e57
AbyssLink <26038696+AbyssLink@users.noreply.github.com>
2020-05-07 16:52:25 +0800

    add  pagination support

--- club/views.py
+++ club/views.py
@@ -20,6 +20,7 @@ class PostListView(ListView):
     context_object_name = 'posts'
     # inverse order as date_posted
     ordering = ['-date_posted']
+    paginate_by = 5
 
 
 class PostDetailView(DetailView):
